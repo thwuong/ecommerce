25df20e99e30f978ca63adfc1ec83c37ba63c598
thwuong <duongthuong654@gmail.com>
2024-06-06 17:30:12 +0700

    fix: missing package

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import acceptLanguage from "accept-language";
+import { NextRequest, NextResponse } from "next/server";
+import { cookieName, fallbackLng, languages } from "./src/app/i18n/settings";
+
+acceptLanguage.languages(languages);
+
+export const config = {
+    // matcher: '/:lng*'
+    matcher: [
+        "/((?!api|_next/static|_next/image|assets|favicon|robots.txt|sitemap.xml|favicon.ico|sw.js|site.webmanifest).*)",
+    ],
+};
+
+export function middleware(req: NextRequest) {
+    let lng;
+    if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
+    if (!lng) lng = acceptLanguage.get(req.headers.get("Accept-Language"));
+    if (!lng) lng = fallbackLng;
+
+    // Redirect if lng in path is not supported
+    if (!languages.some((loc) => req.url.endsWith(`?lang=${loc}`)) && !req.nextUrl.pathname.startsWith("/_next")) {
+        return NextResponse.redirect(new URL(`${req.nextUrl.pathname}?lang=${lng}`, req.url));
+    }
+
+    if (req.headers.has("referer")) {
+        const refererUrl = new URL(req.headers.get("referer") || "");
+        const lngInReferer = languages.find((l) => refererUrl.href.endsWith(`?lang=${l}`));
+        const response = NextResponse.next();
+        if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+        return response;
+    }
+
+    return NextResponse.next();
+}
