50436b8e89a675b57d2984e820cb0176ae7cad98
thwuong <duongthuong654@gmail.com>
2024-06-14 09:13:07 +0700

    feat: add page products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,7 +4,7 @@ import { Products } from "@/components/product";
 
 export default async function Page() {
     return (
-        <main>
+        <main className="bg-background-primary">
             <section className="py-[100px] w-full">
                 <Container>
                     <GiftCard />
